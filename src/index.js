4fc4d5e41a49ab67c4b131c2fbf0ff462cd7baf1
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-20 15:17:24 +0200

    Defining & Using Routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // npm install react-router-dom@5
-// 269 Defining & Using Routes
+// 269 
