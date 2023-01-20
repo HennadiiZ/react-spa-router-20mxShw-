4a13db4acec62ab184bb1f2fcbb2dce3ecb05733
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-20 17:52:06 +0200

    Using _Switch_ and _exact_ For Configuring Routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Products = () => {
   return (
     <>
       <h1>THE products PAGE</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <NavLink to='/products/p1'>Product 1</NavLink>
+        </li>
+        <li>
+          <NavLink to='/products/p2'>Product 2</NavLink>
+        </li>
+        <li>
+          <NavLink to='/products/p3'>Product 3</NavLink>
+        </li>
       </ul>
     </>
   );
